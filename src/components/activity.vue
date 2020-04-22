2bfd3a07ea836b9f308f92328a7ac155d7db1e50
AnandKumarRajpal <ak05173@st.habib.edu.pk>
2020-04-22 22:26:18 +0500

    Basic Template

---
+++ src/components/activity.vue
@@ -0,0 +1,32 @@
+<template>
+    <article class="post">
+        <h4>{{item.title}}</h4>
+        <div class="media">
+        <div class="media-left">
+            <p class="image is-32x32">
+            <img src="../assets/user.png">
+            </p>
+        </div>
+        <div class="media-content">
+            <div class="content">
+            <p>
+                <a href="#">Filip Jerga</a> updated {{item.updatedAt}} minutes ago &nbsp;
+            </p>
+            </div>
+        </div>
+        <div class="media-right">
+            <span>{{item.progress}}</span>
+        </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ["item"],
+}
+</script>
+
+<style scoped>
+
+</style>
